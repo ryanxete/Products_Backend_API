acaea1ca6c263dc19230da78eaa5725cea0aaa79
Ryan Etedali <ryanetedali@gmail.com>
2022-07-14 11:04:53 -0500

    added Car Model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    brand = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=50)
+    inventory = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
